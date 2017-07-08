8d5895bb10391990b6846274ba3290f8456c3c16
lordjbs <stebojo262@gmail.com>
2017-07-08 17:18:17 +0200

    please work.

--- sandboxed.js
+++ sandboxed.js
@@ -3,13 +3,30 @@ const client = require("./client.js");
 const sandboxed = new client();
 const utils = require("./utils.js");
 const fs = require("fs");
+var guilds_collection = new Discord.Collection();
 
 sandboxed.on("ready", () => {
   utils.log("Ready!");
+  sandboxed.user.setGame(">>help | " + sandboxed.guilds.size + "servers <3")
+
+  sandboxed.color = 0x33cccc;
+  sandboxed.utils = require("./utils.js");
+  sandboxed.embedFooter = "sandboxed | coded with ❤ by @jbs#3049";
+  sandboxed.version = "2.0.1"
+  sandboxed.codename = "smoothie"
+  sandboxed.config = require("../config.json")
+  sandboxed.guilds = guilds_collection;
+  sandboxed.ownerid = "220591718158827520";
+  sandboxed.guilds.forEach(x => {
+    var id = x.id
+    guilds_collection.set("" + id, {name: x.name, owner: x.owner, channels: x.channels, users: x.members, member_count: x.members.size, channel_count: x.channels.size})
+  })
+}
+
 });
 
 sandboxed.on("message", msg => {
-  var prefix = "this.";
+  var prefix = ">>";
   if (!msg.content.startsWith(prefix)) return false;
   if(msg.author.bot) return false;
 
